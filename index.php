41a5485860faef7a961423995783cee0b177a369
Roman Efimov <romaonthego@gmail.com>
2011-03-06 13:31:40 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once "lib/PaypalExpressCheckout.php";
+
+$gateway = new PaypalGateway();
+$gateway->apiUsername = "YOUR API USERNAME HERE";
+$gateway->apiPassword = "YOUR API PASSWORD HERE";
+$gateway->apiSignature = "YOUR API SIGNATURE HERE";
+$gateway->testMode = true;
+
+// Return (success) and cancel url setup
+$gateway->returnUrl = "http://test.site/?action=success";
+$gateway->cancelUrl = "http://test.site/?action=cancel";
+
+$paypal = new PaypalExpressCheckout($gateway);
+
+switch ($_GET['action']) {
+    case "": // Index page, here you should be redirected to Paypal
+        $paypal->doExpressCheckout(123.45, 'Test service', 'inv123', 'USD', $resultData);
+        break;
+    
+    case "success": // Paypal says everything's fine, do the charge (user redirected to $gateway->returnUrl)
+        if ($paypal->doPayment($_GET['token'], $_GET['PayerID'], $resultData))
+            echo "Success!"; else {
+                echo "Debugging what went wrong: ";
+                print_r($resultData);
+            }
+        break;
+    
+    case "cancel": // User canceled and returned to your store (to $gateway->cancelUrl)
+        echo "User canceled";
+        break;
+}
+
+?>
---
+++ lib/PaypalExpressCheckout.php
@@ -0,0 +1,106 @@
+<?php
+//  PaypalExpressCheckout.php
+//  PaypalExpressCheckout
+//
+// Copyright 2011 Roman Efimov <romefimov@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+require_once "common/PaypalBase.php";
+
+class PaypalExpressCheckout extends PaypalBase {
+    
+    /**
+     * Redirect user to PayPal to request payment permissions
+     * 
+     * If OK, the customer is redirected to PayPal gateway
+     * If error, returns false
+     * 
+     * @param float $amount Amount (2 numbers after decimal point)
+     * @param string $desc Item description
+     * @param string $invoice Invoice number (can be omitted)
+     * @param string $currency 3-letter currency code (USD, GBP, CZK etc.)
+     * @param array $resultData PayPal response
+     * 
+     * @return bool
+     */
+    public function doExpressCheckout($amount, $description, $invoice = '', $currency = 'USD', &$resultData = array()){
+        $data = array('PAYMENTACTION' =>'Sale',
+                      'AMT' => $amount,
+                      'DESC' => $description,
+                      'NOSHIPPING' => "1",
+                      'ALLOWNOTE' => "0",
+                      'CURRENCYCODE' => $currency,
+                      'METHOD' => 'SetExpressCheckout');
+
+        $data['CUSTOM'] = $amount.'|'.$currency.'|'.$invoice;
+        if ($invoice) $data['INVNUM'] = $invoice;
+
+        if (!$resultData = $this->runQueryWithParams($data)) return false;
+        if ($resultData['ACK'] == 'FAILURE') return false;
+
+        if ($resultData['ACK'] == 'SUCCESS') {
+            header('Location: '.$this->gateway->getGate().'cmd=_express-checkout&useraction=commit&token='.$resultData['TOKEN']);
+            exit();
+        }
+        return false;
+    }
+    
+    /**
+     * Gets checkout information from PayPal
+     * 
+     * @param string $token PayPal token
+     * 
+     * @return array $resultData PayPal response
+     */
+    protected function getCheckoutDetails($token) {
+        $data = array('TOKEN' => $token,
+                      'METHOD' =>'GetExpressCheckoutDetails');
+        
+        if (!$resultData = $this->runQueryWithParams($data)) return false;
+        return $resultData;
+    }
+    
+    /**
+     * Perform payment based on token and Payer ID
+     * 
+     * If OK, returns true
+     * If error, returns false
+     * 
+     * @param string $token PayPal token returned with GET response
+     * @param string $payerId PayPal Payer ID returned with GET response
+     * @param array $resultData PayPal response
+     * 
+     * @return bool
+     */
+    public function doPayment($token, $payerId, &$resultData = array()) {
+        $details = $this->getCheckoutDetails($token);
+        if (!$details) return false;
+        list($amount, $currency, $invoice) = explode('|', $details['CUSTOM']);
+        $data = array('PAYMENTACTION' => 'Sale',
+                      'PAYERID' => $payerId,
+                      'TOKEN' => $token,
+                      'AMT' => $amount,
+                      'CURRENCYCODE' => $currency,
+                      'METHOD' =>'DoExpressCheckoutPayment');
+        
+        if (!$resultData = $this->runQueryWithParams($data)) return false;
+        if ($resultData['ACK'] == 'SUCCESS') return true;
+        return false;
+    }
+    
+}
+
+
+?>
